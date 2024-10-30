37c8e018bc34ab34398578a8a7f8e700a7a393f7
Ventsislav Saraminev <v.saraminev@abv.bg>
2024-10-30 12:56:15 +0200

    Proshop
    Modeling database;
    Added routing;
    Added seeder;

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-import products from './data/products.js';
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+connectDB();
 
 const port = process.env.PORT;
 
- const app = express();
+const app = express();
 
- app.get('/', (req, res) => {
-    res.send('API is running....');
- });
+app.get('/', (req, res) => {
+   res.send('API is running....');
+});
 
- app.get('/api/products', (req, res) => {
-    res.json(products);
- });
+app.use('/api/products', productRoutes);
 
- app.get('/api/products/:id', (req, res) => {
-    res.json(products.find(product => product._id === req.params.id))
- });
+app.use(notFound)
+app.use(errorHandler);
 
- app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
